f602bc8e63c8ad1abcc8a7af6e9a1c06d6c33ee3
Javier <60045661+javier-hub@users.noreply.github.com>
2023-09-22 19:10:41 -0300

    Update DBtrailerflix_insert.sql

--- db/DBtrailerflix_insert.sql
+++ db/DBtrailerflix_insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO `bntaiiicjkgrbw5jdfji`.`actores`
+INSERT INTO `trailerflix`.`actores`
 (`idActor`,
 `nombreActor`)
 VALUES
@@ -213,7 +213,7 @@ VALUES
 (209,"Burn Gorman");
 
 
-INSERT INTO `bntaiiicjkgrbw5jdfji`.`géneros`
+INSERT INTO `trailerflix`.`géneros`
 (`idGenero`,
 `nombreGenero`)
 VALUES
@@ -239,7 +239,7 @@ VALUES
 (20,"¿Ficción?"),
 (21,"Hechos veríficos");
 
-INSERT INTO `bntaiiicjkgrbw5jdfji`.`categorías`
+INSERT INTO `trailerflix`.`categorías`
 (`idCategoria`,
 `nombreCategoria`)
 VALUES
@@ -248,7 +248,7 @@ VALUES
 (2,
 "Serie");
 
-INSERT INTO `bntaiiicjkgrbw5jdfji`.`contenidos`
+INSERT INTO `trailerflix`.`contenidos`
 (`idContenido`,
 `posterContenido`,
 `tituloContenido`,
@@ -294,7 +294,7 @@ VALUES
 (34,"/posters/34.jpg","El primer hombre en la luna","1","Cuenta la historia de la misión de la NASA que llevó al primer hombre a la luna, centrada en Neil Armstrong (interpretado por Ryan Gosling) y el periodo comprendido entre los años 1961 y 1969. Un relato en primera persona, basado en la novela de James R. Hansen, que explora el sacrificio y el precio que representó, tanto para Armstrong como para Estados Unidos, una de las misiones más peligrosas de la historia.","N/A",""),
 (35,"/posters/35.jpg","Titanes del pacífico - La insurrección","1","Han pasado 10 años tras la primera invasión que sufrió la humanidad, pero la lucha aún no ha terminado. El planeta vuelve a ser asediado por los Kaiju, una raza de alienígenas colosales, que emergen desde un portal interdimensional con el objetivo de destruir a la raza humana. Ante esta nueva amenaza, los Jaegers, robots gigantes de guerra pilotados por dos personas para sobrellevar la inmensa carga neuronal que conlleva manipularlos, ya no están a la altura de lo que se les viene encima. Será entonces cuando los supervivientes de la primera invasión, además de nuevos personajes como el hijo de Pentecost, tendrán que idear la manera de sorprender al enorme enemigo, apostando por nuevas estrategias defensivas y de ataque. Con la Tierra en ruinas e intentando reconstruirse, esta nueva batalla puede ser decisiva para el futuro.","N/A","");
 
-INSERT INTO `bntaiiicjkgrbw5jdfji`.`contenidogenero`
+INSERT INTO `trailerflix`.`contenidogenero`
 (`idContenidoGenero`,
 `idContenido`,
 `idGenero`)
@@ -396,7 +396,7 @@ VALUES
 (NULL,35,2),
 (NULL,35,11);
 
-INSERT INTO `bntaiiicjkgrbw5jdfji`.`contenidoactor`
+INSERT INTO `trailerflix`.`contenidoactor`
 (`idContenidoActor`,
 `idContenido`,
 `idActor`)
